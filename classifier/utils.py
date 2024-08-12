57104938f1c976eb8b70fdb96177739e2e66b757
Abdul Kola AbdurRaheem <54320228+kolabdu@users.noreply.github.com>
2024-08-12 16:07:08 +0100

    Update utils.py

--- classifier/utils.py
+++ classifier/utils.py
@@ -1,38 +1,158 @@
 import pickle
 
+# def clean_url(url):
+#     prefixes = ['http://www.', 'https://www.', 'http://', 'https://', 'www.']
+#     for prefix in prefixes:
+#         if url.startswith(prefix):
+#             url = url.replace(prefix, '', 1)
+#             break
+#     return cleaned_url
+
+# def tokenize(cleaned_url):
+#     tkns_BySlash = cleaned_url.split('/')
+#     total_Tokens = []
+#     for i in tkns_BySlash:
+#         tokens = i.split('-')
+#         tkns_ByDot = []
+#         for token in tokens:
+#             temp_Tokens = token.split('.')
+#             tkns_ByDot.extend(temp_Tokens)
+#         total_Tokens.extend(tokens + tkns_ByDot)
+#     total_Tokens = list(set(total_Tokens))
+#     if 'com' in total_Tokens:
+#         total_Tokens.remove('com')
+#     return cleaned_url
+
+# def load_model_and_vectorizer():
+#     with open('classifier/logit_model.pkl', 'rb') as file:
+#         model = pickle.load(file)
+
+#     with open('classifier/vectorizer.pkl', 'rb') as file:
+#         vectorizer = pickle.load(file)
+        
+#     return model, vectorizer
+
+# def predict_url_category(cleaned_url, model, vectorizer):
+#     X = vectorizer.transform(cleaned_url)
+#     prediction = model.predict(X)[0]
+#     return prediction
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import pickle
+# import dill
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# #from . import vectorizer_rename as vector
+
+# def clean_url(url):
+#     prefixes = ['http://www.', 'https://www.', 'www.']
+#     for prefix in prefixes:
+#         if url.startswith(prefix):
+#             url = url.replace(prefix, '')
+#             break
+#     return url
+
+
+# # Define the makeTokens function
+# def makeTokens(f):
+#     tkns_BySlash = str(f.encode('utf-8')).split('/')    # make tokens after splitting by slash
+#     total_Tokens = []
+#     for i in tkns_BySlash:
+#         tokens = str(i).split('-')    # make tokens after splitting by dash
+#         tkns_ByDot = []
+#         for j in range(0, len(tokens)):
+#             temp_Tokens = str(tokens[j]).split('.')    # make tokens after splitting by dot
+#             tkns_ByDot = tkns_ByDot + temp_Tokens
+#         total_Tokens = total_Tokens + tokens + tkns_ByDot
+#     total_Tokens = list(set(total_Tokens))    # remove redundant tokens
+#     if 'com' in total_Tokens:
+#         total_Tokens.remove('com')    # removing .com since it occurs a lot of times and it should not be included in our features
+#     return total_Tokens
+
+# # Initialize TfidfVectorizer with custom tokenizer
+# vectorizer = TfidfVectorizer(tokenizer=makeTokens)
+
+# with open('vectorizer_dill.pkl', 'wb') as file:
+#     dill.dump(vectorizer, file)
+
+
+# def load_model_and_vectorizer():
+#     with open('logit_model.pkl', 'rb') as file:
+#         model = pickle.load(file)
+#     with open('vectorizer_dill.pkl', 'rb') as file:
+#         vectorizer = dill.load(file)
+#     return model, vectorizer
+
+# def predict_url_category(url, model,vectorizer):
+       
+#     prediction = model.predict(vectorizer.transform(url))
+#     if prediction == 'good':
+#         return 'The URL is safe'
+#     else:
+#         return "URL APPEARS TO BE MALICIOUS"
+
+
+
+# import dill
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# #from . import vectorizer_rename as vector
+
 def clean_url(url):
-    prefixes = ['http://www.', 'https://www.', 'http://', 'https://', 'www.']
+    prefixes = ['http://www.', 'https://www.', 'www.']
     for prefix in prefixes:
         if url.startswith(prefix):
-            url = url.replace(prefix, '', 1)
+            url = url.replace(prefix, '')
             break
-    return cleaned_url
+    return url
 
-def tokenize(cleaned_url):
-    tkns_BySlash = cleaned_url.split('/')
+
+# Define the makeTokens function
+def tokenize(f):
+    tkns_BySlash = str(f.encode('utf-8')).split('/')    # make tokens after splitting by slash
     total_Tokens = []
     for i in tkns_BySlash:
-        tokens = i.split('-')
+        tokens = str(i).split('-')    # make tokens after splitting by dash
         tkns_ByDot = []
-        for token in tokens:
-            temp_Tokens = token.split('.')
-            tkns_ByDot.extend(temp_Tokens)
-        total_Tokens.extend(tokens + tkns_ByDot)
-    total_Tokens = list(set(total_Tokens))
+        for j in range(0, len(tokens)):
+            temp_Tokens = str(tokens[j]).split('.')    # make tokens after splitting by dot
+            tkns_ByDot = tkns_ByDot + temp_Tokens
+        total_Tokens = total_Tokens + tokens + tkns_ByDot
+    total_Tokens = list(set(total_Tokens))    # remove redundant tokens
     if 'com' in total_Tokens:
-        total_Tokens.remove('com')
-    return cleaned_url
+        total_Tokens.remove('com')    # removing .com since it occurs a lot of times and it should not be included in our features
+    return total_Tokens
+
+# Initialize TfidfVectorizer with custom tokenizer
+# vectorizer = TfidfVectorizer(tokenizer=makeTokens)
+
+
+
 
 def load_model_and_vectorizer():
-    with open('classifier/logit_model.pkl', 'rb') as file:
+    with open('logit_model.pkl', 'rb') as file:
         model = pickle.load(file)
-
-    with open('classifier/vectorizer.pkl', 'rb') as file:
-        vectorizer = pickle.load(file)
-        
+    with open('tfidf_vectorizer.pkl', 'rb') as f:
+        vectorizer = pickle.load(f)
     return model, vectorizer
 
-def predict_url_category(cleaned_url, model, vectorizer):
-    X = vectorizer.transform(cleaned_url)
-    prediction = model.predict(X)[0]
-    return prediction
+def predict_url_category(url, model,vectorizer):
+       
+    prediction = model.predict(vectorizer.transform(url))
+    if prediction == 'good':
+        return 'The URL is safe'
+    else:
+        return "URL APPEARS TO BE MALICIOUS"
+
+
+
+
