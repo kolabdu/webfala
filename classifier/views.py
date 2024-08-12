ed45e5f115a315768de6c5e26424d55a92587c6c
Abdul Kola AbdurRaheem <54320228+kolabdu@users.noreply.github.com>
2024-08-12 19:08:59 +0100

    Update views.py

--- classifier/views.py
+++ classifier/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
-from .utils import clean_url,load_model_and_vectorizer, predict_url_category
+from .utils import tokenize, load_model_and_vectorizer, predict_url_category
 
 def index(request):
     return render(request, 'index.html')
@@ -11,8 +11,7 @@ def index(request):
 def predict_category(request):
     if request.method == 'POST':
         user_input = request.POST.get('url_input')
-        cleaned_input = [clean_url(user_input)]
-        # cleaned_input = [tokenize(cleaned_url)]
+        cleaned_input = [tokenize(user_input)]
 
         model, vectorizer = load_model_and_vectorizer()
         try:
